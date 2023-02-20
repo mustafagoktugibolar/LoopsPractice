e75a1f78b2af33951f05301fb5cac6534a78361a
mustafagoktugibolar <mustafagoktugibolar@gmail.com>
2023-02-20 19:21:53 +0300

    Combination Done

---
+++ src/Combination.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Combination {
+    private static int factoriel(int input){
+        int result = 1;
+            for(int i = 1; i <= input; i++){
+                result*=i;
+            }
+
+        return result;
+    }
+    public static void find(){
+        Scanner sc = new Scanner(System.in);
+        // C(n,r) = n! / (r! * (n-r)!)
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        System.out.print("Enter r : ");
+        int r = sc.nextInt();
+
+        int result = factoriel(n) / (factoriel(n-r) * factoriel(r));
+
+        System.out.println("Combination of (" 
+        +n + "," + r + ") is " + result);
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         PowersOf4and5.find();
+        Combination.find();
+        
     }
 }
