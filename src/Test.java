d4b88278de6ec561a69f69f200a30c129fa6e4b5
mustafagoktugibolar <mustafagoktugibolar@gmail.com>
2023-02-21 13:32:41 +0300

    Triangel added

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,11 @@
 public class Test {
     public static void main(String[] args) {
         //PowersOf4and5.find();
-        //Combination.find();
         //MaxMin.find();
         //UsluSayi.find();
         //BasamakToplami.find();
-        HarmonikToplam.find();
+        //HarmonikToplam.find();
+        Triangel.find();
         
     }
 }
